a2ef1681376dcd39a6796649342f169c4f96e984
syamilisn <shyamilisn.itfa@gmail.com>
2024-04-03 10:16:09 +0530

    removed names

--- hello.c
+++ hello.c
@@ -3,13 +3,7 @@ void main(){
     printf("Hello world \n");
     printf("heyo \n");
 
-    printf("i am shyamilisn \n");
-    printf("Im Surya\n");
-    printf("Im Shalini\n");
-    printf("I am sudha\n");
-    printf("I AM AKHIL RAJ");
-    printf("hello\n");
-    printf("haii idiots\n");
+ printf("haii idiots\n");
     
     printf("[patch1] added line");
 }
